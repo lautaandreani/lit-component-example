b7667b033e62373830e83d749e3b645c3e0f1ae1
lautaandreani <lautaroandreani2016@gmail.com>
2023-08-14 14:50:34 -0300

    changes: vite build changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import path from 'path'
+import fs from 'fs'
+
+const componentPath = path.join(__dirname, './src/components/atoms');
+const componentFolders = fs.readdirSync(componentPath, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
 
 export default defineConfig({
   resolve: {
@@ -10,13 +16,15 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      entry: Object.fromEntries(
+        componentFolders.map(folderName => [folderName, `./src/components/atoms/${folderName}/index.ts`])
+      ),
       formats: ['es'],
     },
     rollupOptions: {
       external: [/^lit/],
       output: {
-        entryFileNames: `components/[name]/[name].js`,
+        entryFileNames: `components/atoms/[name].js`,
         format: 'es',
       },
     },
